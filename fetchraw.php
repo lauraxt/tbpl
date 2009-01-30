51704fb483c4ceb7f8eccdc64562ac10ea787f98
Markus Stange <mstange@themasta.com>
2009-01-31 00:21:50 +0100

    Improve loading time by removing most external references

--- fetchraw.php
+++ fetchraw.php
@@ -6,4 +6,14 @@ $tree = isset($_GET["tree"]) ? $_GET["tree"] : "Firefox";
 $url = $site == "tinderbox" ? "http://tinderbox.mozilla.org/" . $tree . "/" : ($tree == "Firefox3.1" ? "http://hg.mozilla.org/releases/mozilla-1.9.1/pushloghtml?startdate=16+hours+ago&enddate=now" : ($tree == "TraceMonkey" ? "http://hg.mozilla.org/tracemonkey/pushloghtml?startdate=16+hours+ago&enddate=now" : "http://hg.mozilla.org/mozilla-central/pushloghtml?startdate=16+hours+ago&enddate=now"));
 
 header("Content-Type: text/html,charset=utf-8");
-echo preg_replace("/\<iframe.*\/iframe>/Ui", "", file_get_contents($url));
+
+$code = file_get_contents($url);
+$code = preg_replace("/<link.*>/Ui", "", $code);
+if ($site == "tinderbox") {
+    $code = preg_replace("/<img.*>/Ui", "",
+            preg_replace("/<style.*\/style>/Ui", "",
+            preg_replace("/<iframe.*\/iframe>/Ui", "",
+                         $code)));
+}
+
+echo $code;
