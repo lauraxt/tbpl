eb89b644b406c3a7f9ddd97f5915d20a3e5dc109
Markus Stange <mstange@themasta.com>
2009-05-25 20:53:09 +0200

    Refactoring (forgot to add Config.js)

---
+++ Config.js
@@ -0,0 +1,13 @@
+var Config = {
+  tinderboxDataLoader: TinderboxJSONUser,
+  pushlogDataLoader: PushlogHTMLParser,
+  repoNames: {
+    "Firefox": "mozilla-central",
+    "Firefox3.5": "releases/mozilla-1.9.1",
+    "TraceMonkey": "tracemonkey",
+    "Thunderbird": "comm-central",
+    "Thunderbird3.0": "comm-central",
+    "SeaMonkey": "comm-central",
+    "Sunbird": "comm-central",
+  },
+};
