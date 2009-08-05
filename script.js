cdb77920aee100550c4476ef75bf28b99051cc96
Markus Stange <mstange@themasta.com>
2009-08-05 17:49:48 +1200

    Complain about invalid tree names and list the valid ones.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-if (checkPreqs())
-  Controller.init();
+try {
+  if (checkPreqs())
+    Controller.init();
+} catch(e) {
+  if (e == "wrongtree") {
+    location.href = "wrongtree.html" + location.search;
+  }
+}
