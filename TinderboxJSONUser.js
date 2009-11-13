74b4c2b3517bfa96b1056a2a873eff5c8c98c4ec
Markus Stange <mstange@themasta.com>
2009-11-13 21:39:20 +0100

    Parse build scrape only when it's needed to make Tinderbox JSON parsing a little faster

--- TinderboxJSONUser.js
+++ TinderboxJSONUser.js
@@ -55,6 +55,151 @@ var TinderboxJSONUser = {
     return note.replace(/<\/?pre>/g, "").trim().replace(/\n/g, "<br>");
   },
   
+  findRevInScrape: function(scrape) {
+    if (!scrape)
+      return "";
+    var cell = document.createElement("td");
+    cell.innerHTML = scrape.join("<br>\n");
+    var revLinks = $('a[href^="http://hg.mozilla.org"]', cell).get();
+    for (var i = 0; i < revLinks.length; i++) {
+      var linkText = revLinks[i].textContent;
+      if (linkText.indexOf("mobile") == -1) {
+        return linkText.substr(4, 12);
+      }
+    }
+
+    // Tryserver uses try-c49054c95eba instead
+    match = scrape.join("").match(/try\-([0-9a-f]{12})/);
+    return match ? match[1] : null;
+  },
+  
+  getBuildScrape: function(td, machineRunID) {
+    return td.scrape[machineRunID];
+  },
+  
+  parseTinderbox: function(tree, td) {
+    var self = this;
+    var machines = [];
+    $(td.build_names).each(function(i, name) {
+      var machinetype = self.getMachineType(name);
+      if (!machinetype.os || !machinetype.type) {
+        return;
+      }
+      machines[i] = {
+        "name": name,
+        "os": machinetype.os,
+        "type": machinetype.type,
+        latestFinishedRun: { id: "", startTime: -1 },
+        "runs": 0,
+        "runtime": 0
+      };
+    });
+  
+    var notes = td.note_array.map(self.processNote);
+  
+    var machineResults = {};
+    td.build_table.forEach(function(row) { row.forEach(function(build, machineIndex) {
+      if (!build.buildstatus || build.buildstatus == "null" || !machines[machineIndex])
+        return;
+      var state = build.buildstatus; /* building, success, testfailed, busted */
+      var rev = "";
+      var startTime = new Date(build.buildtime * 1000);
+      var endTime = (state != "building") ? new Date(build.endtime * 1000) : 0;
+      var machineRunID = build.logfile;
+      var buildScrape = self.getBuildScrape(td, machineRunID);
+      var rev = self.findRevInScrape(buildScrape);
+  
+      if (machineResults[machineRunID])
+        return;
+  
+      if (state != "building" && !rev)
+        return;
+  
+      var note = build.hasnote ? notes[build.noteid * 1] : "";
+  
+      if (state == 'success' && endTime) {
+        machines[machineIndex].runs++;
+        machines[machineIndex].runtime+=
+          (endTime.getTime() - startTime.getTime())/1000;
+      }
+  
+      machineResults[machineRunID] = new MachineResult ({
+        "tree" : tree,
+        "machine": machines[machineIndex],
+        "runID": machineRunID,
+        "fullLogURL": self.getLogURL(tree, machineRunID, true, false),
+        "briefLogURL": self.getLogURL(tree, machineRunID, false, false),
+        "addNoteURL": self.getLogURL(tree, machineRunID, false, true),
+        "state": state,
+        "startTime": startTime,
+        "endTime": endTime,
+        "rev": rev,
+        "guessedRev": rev,
+        "note": note,
+        "errorParser": build.errorparser,
+        "_scrape": buildScrape,
+      });
+      if (state != "building") {
+        if (startTime.getTime() > machines[machineIndex].latestFinishedRun.startTime) {
+          machines[machineIndex].latestFinishedRun = {
+            id: machineRunID,
+            "startTime": startTime.getTime()
+          };
+        }
+      }
+    }); });
+  
+    machines.forEach(function(machine) {
+      if (machine.runs) {
+        machine.averageCycleTime = Math.ceil(machine.runtime/machine.runs);
+      }
+      delete machine.runs;
+      delete machine.runtime;
+    });
+  
+    return { "machines": machines, "machineResults": machineResults };
+  }
+};
+
+if (!String.prototype.trim) {
+  String.prototype.trim = function () {
+    var x=this;
+    x=x.replace(/^\s*(.*?)/, "$1");
+    x=x.replace(/(.*?)\s*$/, "$1");
+    return x;
+  }
+}
+
+function MachineResult(data) {
+  for (var i in data) {
+    this[i] = data[i];
+  }
+}
+
+MachineResult.prototype = {
+  getTestResults: function () {
+    var self = this;
+    var machine = this.machine;
+    var scrape = this._scrape;
+    if (!scrape)
+      return [];
+    return (function (fun) {
+      return (fun[machine.type] ? fun[machine.type] : fun.generic).call(self, scrape);
+    })({
+      "Unit Test": self.getUnitTestResults,
+      "Mochitest": self.getUnitTestResults,
+      "Opt Mochitest": self.getUnitTestResults,
+      "Debug Mochitest": self.getUnitTestResults,
+      "Opt Everythingelse Test": self.getUnitTestResults,
+      "Debug Everythingelse Test": self.getUnitTestResults,
+      "Everythingelse Test": self.getUnitTestResults,
+      "Talos": self.getTalosResults,
+      "Leak Test": self.getScrapeResults,
+      "Build": self.getScrapeResults,
+      "generic": self.getScrapeResults
+    });
+  },
+  
   getScrapeResults: function(scrape) {
     return $(scrape).map(function() {
       if (this.match(/rev\:/) || this.match(/s\:/) || this.match(/try\-/))
@@ -105,138 +250,4 @@ var TinderboxJSONUser = {
       };
     }).filter(function() { return this; }).get();
   },
-  
-  findRevInScrape: function(scrape) {
-    var cell = document.createElement("td");
-    cell.innerHTML = scrape.join("<br>\n");
-    var revLinks = $('a[href^="http://hg.mozilla.org"]', cell).get();
-    for (var i = 0; i < revLinks.length; i++) {
-      var linkText = revLinks[i].textContent;
-      if (linkText.indexOf("mobile") == -1) {
-        return linkText.substr(4, 12);
-      }
-    }
-
-    // Tryserver uses try-c49054c95eba instead
-    match = scrape.join("").match(/try\-([0-9a-f]{12})/);
-    return match ? match[1] : null;
-  },
-  
-  getBuildScrape: function(td, machine, machineRunID) {
-    var self = this;
-    var scrape = td.scrape[machineRunID];
-    if (!scrape)
-      return null;
-    return {
-      "rev": self.findRevInScrape(scrape),
-      "testResults": (function (fun) {
-          return (fun[machine.type] ? fun[machine.type] : fun.generic).call(self, scrape);
-        })({
-          "Unit Test": self.getUnitTestResults,
-          "Mochitest": self.getUnitTestResults,
-          "Opt Mochitest": self.getUnitTestResults,
-          "Debug Mochitest": self.getUnitTestResults,
-          "Opt Everythingelse Test": self.getUnitTestResults,
-          "Debug Everythingelse Test": self.getUnitTestResults,
-          "Everythingelse Test": self.getUnitTestResults,
-          "Talos": self.getTalosResults,
-          "Leak Test": self.getScrapeResults,
-          "Build": self.getScrapeResults,
-          "generic": self.getScrapeResults
-        })
-    };
-  },
-  
-  parseTinderbox: function(tree, td) {
-    var self = this;
-    var machines = [];
-    $(td.build_names).each(function(i, name) {
-      var machinetype = self.getMachineType(name);
-      if (!machinetype.os || !machinetype.type) {
-        return;
-      }
-      machines[i] = {
-        "name": name,
-        "os": machinetype.os,
-        "type": machinetype.type,
-        latestFinishedRun: { id: "", startTime: -1 },
-        "runs": 0,
-        "runtime": 0
-      };
-    });
-  
-    var notes = td.note_array.map(self.processNote);
-  
-    var machineResults = {};
-    td.build_table.forEach(function(row) { row.forEach(function(build, machineIndex) {
-      if (!build.buildstatus || build.buildstatus == "null" || !machines[machineIndex])
-        return;
-      var state = build.buildstatus; /* building, success, testfailed, busted */
-      var rev = "", testResults = [];
-      var startTime = new Date(build.buildtime * 1000);
-      var endTime = (state != "building") ? new Date(build.endtime * 1000) : 0;
-      var machineRunID = build.logfile;
-      var buildScrape = self.getBuildScrape(td, machines[machineIndex], machineRunID);
-      var rev = buildScrape ? buildScrape.rev : "";
-      var testResults = buildScrape ? buildScrape.testResults : [];
-  
-      if (machineResults[machineRunID])
-        return;
-  
-      if (state != "building" && !rev)
-        return;
-  
-      var note = build.hasnote ? notes[build.noteid * 1] : "";
-  
-      if (state == 'success' && endTime) {
-        machines[machineIndex].runs++;
-        machines[machineIndex].runtime+=
-          (endTime.getTime() - startTime.getTime())/1000;
-      }
-  
-      machineResults[machineRunID] = {
-        "tree" : tree,
-        "machine": machines[machineIndex],
-        "runID": machineRunID,
-        "fullLogURL": self.getLogURL(tree, machineRunID, true, false),
-        "briefLogURL": self.getLogURL(tree, machineRunID, false, false),
-        "addNoteURL": self.getLogURL(tree, machineRunID, false, true),
-        "state": state,
-        "startTime": startTime,
-        "endTime": endTime,
-        "rev": rev,
-        "guessedRev": rev,
-        "testResults": testResults,
-        "note": note,
-        "errorParser": build.errorparser,
-      };
-      if (state != "building") {
-        if (startTime.getTime() > machines[machineIndex].latestFinishedRun.startTime) {
-          machines[machineIndex].latestFinishedRun = {
-            id: machineRunID,
-            "startTime": startTime.getTime()
-          };
-        }
-      }
-    }); });
-  
-    machines.forEach(function(machine) {
-      if (machine.runs) {
-        machine.averageCycleTime = Math.ceil(machine.runtime/machine.runs);
-      }
-      delete machine.runs;
-      delete machine.runtime;
-    });
-  
-    return { "machines": machines, "machineResults": machineResults };
-  }
 };
-
-if (!String.prototype.trim) {
-  String.prototype.trim = function () {
-    var x=this;
-    x=x.replace(/^\s*(.*?)/, "$1");
-    x=x.replace(/(.*?)\s*$/, "$1");
-    return x;
-  }
-}
--- UserInterface.js
+++ UserInterface.js
@@ -498,10 +498,11 @@ var UserInterface = {
       result.fullLogURL + '">View Full Log</a> <a class="addNote" href="' +
       result.addNoteURL + '">Add a Comment</a> <span id="summaryLoader"></span>' +
       (function () {
-        if (!result.testResults.length)
+        var testResults = result.getTestResults();
+        if (!testResults.length)
           return '';
         return '<ul class="testResults">\n' +
-        result.testResults.map(function(r) {
+        testResults.map(function(r) {
           return '<li>' + r.name +
             (r.result ? ': ' + (r.resultURL ? '<a href="' + r.resultURL.replace(/"/g, "&quot;") +
                                               '">' + r.result + '</a>'
