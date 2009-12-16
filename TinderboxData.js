51c78b6d776f7ddccfcc5bb49f0b552650c43092
Markus Stange <mstange@themasta.com>
2009-12-16 16:30:10 +0100

    Bug 534843 - Remove support for separate *-Unittest trees.

--- TinderboxData.js
+++ TinderboxData.js
@@ -2,8 +2,7 @@ function TinderboxData(treeName, dataLoader, repoNames) {
   this._treeName = treeName;
   this._dataLoader = dataLoader;
   this._repoNames = repoNames;
-  this._normalData = { machines: [], machineResults: {} };
-  this._unittestData = { machines: [], machineResults: {} };
+  this._data = { machines: [], machineResults: {} };
 }
 
 TinderboxData.prototype = {
@@ -13,7 +12,6 @@ TinderboxData.prototype = {
   treesWithGroups: {
     "Firefox": ["Mochitest", "Opt Mochitest", "Debug Mochitest"],
   },
-  _treesWithUnittest: ["Firefox", "Firefox3.5", "Firefox3.6", "TraceMonkey"],
 
   getRepoName: function TinderboxData_getRepoName() {
     return this._repoNames[this._treeName];
@@ -24,49 +22,15 @@ TinderboxData.prototype = {
     this._dataLoader.load(
       this._treeName,
       timeOffset,
-      function mainTreeLoadCallback(normalData) {
-        self._normalData = normalData;
+      function mainTreeLoadCallback(data) {
+        self._data = data;
         loadCallback(self.getData());
       },
       failCallback
     );
-    if (this._hasUnittestTree()) {
-      this._dataLoader.load(
-        this._getUnittestTreeName(),
-        timeOffset,
-        function unittestTreeLoadCallback(unittestData) {
-          self._unittestData = unittestData;
-          loadCallback(self.getData());
-        },
-        failCallback
-      );
-    }
   },
 
   getData: function TinderboxData_getData() {
-    return {
-      machines: this._normalData.machines.concat(this._unittestData.machines),
-      machineResults: this._combineObjects(this._normalData.machineResults, this._unittestData.machineResults),
-    };
+    return this._data;
   },
-
-  _combineObjects: function TinderboxData__combineObjects(a, b) {
-    var c = {};
-    for (var i in a) {
-      c[i] = a[i];
-    }
-    for (i in b) {
-      c[i] = b[i];
-    }
-    return c;
-  },
-
-  _hasUnittestTree: function TinderboxData__hasUnittestTree() {
-    return this._treesWithUnittest.indexOf(this._treeName) != -1;
-  },
-
-  _getUnittestTreeName: function TinderboxData__getUnittestTreeName() {
-    return this._treeName + "-Unittest";
-  },
-
 }
