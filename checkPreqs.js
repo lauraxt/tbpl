5906e9c6661dad7cad120ff7cf76efff273613b0
Markus Stange <mstange@themasta.com>
2010-01-14 23:26:50 +0100

    Don't require toLocaleFormat anymore since we don't use it. Now tbpl can be used in Webkit (bug 529611).

--- checkPreqs.js
+++ checkPreqs.js
@@ -1,5 +1,5 @@
 function checkPreqs() {
-  return Date.prototype.toLocaleFormat !== undefined;
+  return true; // XXX need to check what we really need and which browsers support it.
 }
 
 document.body.className = "withJavaScript " + (checkPreqs() ? "with" : "no") + "Firefox3";
