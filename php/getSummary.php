01c7af5d23a1d8a94d0ff209572df35648590cee
Ehsan Akhgari <ehsan@mozilla.com>
2010-06-24 13:50:22 -0400

    Bug 573831 - Implement Input Validation To Prevent Arbitrary File System Read Access; r=arpad

--- php/getSummary.php
+++ php/getSummary.php
@@ -3,6 +3,12 @@
 if (!isset($_GET["tree"]) || !isset($_GET["id"]))
   die("tree or id not set");
 
+if (!preg_match('/^[a-zA-Z0-9-]+$/', $_GET["tree"]))
+  die("invalid tree");
+
+if (!preg_match('/^\d+\.\d+\.\d+\.gz$/', $_GET["id"]))
+  die("invalid id");
+
 if (!isset($_GET["starred"]) || $_GET["starred"] != "true")
   $_GET["starred"] = false;
 else
